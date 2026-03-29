d81af91527d8adf532610bf0bc263727e7b553d4
jcc51189 <jcc51189@gmail.com>
2026-03-29 16:19:02 -0700

    Add files via upload

---
+++ Tic-tac-toe/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('sushi-tac-toe').then(cache => {
+      return cache.addAll(['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png']);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
